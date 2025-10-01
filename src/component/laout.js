2ac9dbd076864959d6b9164efcc18d578baa6b46
Vovan1973 <teresvovan1973@gmail.com>
2025-10-01 22:27:21 +0300

    Добавлен файл 'laout.module.css'

--- src/component/laout.js
+++ src/component/laout.js
@@ -1,9 +1,10 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import {container} from './laout.module.css'
 
 const Layout = ({ pageTitle, nameTitle, children }) => {
   return (
-    <div>
+    <div className={container}>
       <nav>
         <ul>
           <li><Link to="/">Главная</Link></li>
